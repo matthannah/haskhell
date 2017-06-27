aa524dac31f280e883929312f43da3415fe9351c
matthannah <mryanhannah@gmail.com>
2017-06-27 11:50:24 +1000

    QUALITY: Add comments about improvements

--- fp2.0/sessions/1/hanoi.hs
+++ fp2.0/sessions/1/hanoi.hs
@@ -3,11 +3,13 @@ module Hanoi where
 type Peg = String
 type Move = (Peg, Peg)
 
+-- Example:
+-- hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
--- Example:
--- hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
+-- we can replace one of the ++ with the : operator!
+-- hanoi n a b c = hanoi (n - 1) a c b ++ ((a, b) : (hanoi (n - 1) c b a))
 
 -- TODO: hanoiFour
 --hanoiFour :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
