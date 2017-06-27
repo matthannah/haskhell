3c8b601d15ff526b02253d646af097dcc7f5dc39
matthannah <mryanhannah@gmail.com>
2017-06-27 11:20:27 +1000

    QUALITY: Add notes about other solutions

--- fp2.0/sessions/1/cc-valid.hs
+++ fp2.0/sessions/1/cc-valid.hs
@@ -5,6 +5,8 @@ toDigits x
   | x <= 0 = []
   | x < 10 = [x]
   | otherwise = toDigits (x `div` 10) ++ [(x `mod` 10)]
+  -- we can instead define this as:
+  -- toDigits x = reverse.toDigitsRev (this is composing with eta reduction)
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
@@ -15,14 +17,20 @@ doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther x = reverse $ doubleEverySecond $ reverse x
   where doubleEverySecond (x1 : x2 : xs) = [x1, x2 * 2] ++ (doubleEverySecond xs)
         doubleEverySecond (x : []) = [x]
-        doubleEverySecond xs = xs
+        doubleEverySecond xs =  xs
+        -- we replace the last 2 pattern matches with:
+        -- doubleEverySecond xs = xs
+        -- we should use : instead of ++ in the first pattern match:
+        -- x1 : x2 * 2 : doubleEverySecond xs
+        -- we should name the function doubleEveryOther' instead of doubleEverySecond
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
 sumDigits (x:xs)
-  | x > 10 = sumDigits ((toDigits x) ++ xs)
+  | x >= 10 = sumDigits ((toDigits x) ++ xs)
   | otherwise = x + sumDigits xs
+  -- we can instead not use ++ operator by having this line as:
+  -- | x >= 10 = sumDigits (toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate x = value `rem` 10 == 0
-  where value = sumDigits $ doubleEveryOther $ toDigits x
+validate x = (sumDigits $ doubleEveryOther $ toDigits x) `rem` 10 == 0
