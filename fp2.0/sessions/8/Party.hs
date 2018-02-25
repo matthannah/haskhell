15b5393283e15a4eac8ec5247c81d1497db78978
Matt Hannah <mryanhannah@gmail.com>
2018-02-25 23:37:07 +1300

    QUALITY: Use pure over return

--- fp2.0/sessions/8/Party.hs
+++ fp2.0/sessions/8/Party.hs
@@ -50,5 +50,5 @@ glFormat (GL es f) = "Total fun: " ++ show f ++ "\n" ++
 
 -- >>= bind
 main :: IO ()
-main = readFile "company.txt" >>= readIO >>= (return . maxFun) >>= (return . glFormat) >>= putStrLn
+main = readFile "company.txt" >>= readIO >>= (pure . maxFun) >>= (pure . glFormat) >>= putStrLn
 -- TODO: This is definitely wrong... i don't understand monads lol.. yet ;)
