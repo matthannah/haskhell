51a5a3d613dc63c5812172aad3dcf8d900e20a41
Matt Hannah <mryanhannah@gmail.com>
2018-06-06 13:35:52 +1000

    ENHANCE: Add better solutions (round 2)

--- fp2.0/sessions/1/cc-valid.hs
+++ fp2.0/sessions/1/cc-valid.hs
@@ -6,23 +6,36 @@ toDigits x
   | x < 10 = [x]
   | otherwise = toDigits (x `div` 10) ++ [(x `mod` 10)]
   -- we can instead define this as:
-  -- toDigits x = reverse.toDigitsRev (this is composing with eta reduction)
+  -- toDigits = reverse . toDigitsRev (this is composing with eta reduction)
 
+--toDigitsRev :: Integer -> [Integer]
+--toDigitsRev x
+--  | x <= 0 = []
+--  | otherwise = x `mod` 10 : toDigitsRev (x `div` 10)
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x
-  | x <= 0 = []
-  | otherwise = x `mod` 10 : toDigitsRev (x `div` 10)
+toDigitsRev = reverse . toDigits
+
+--doubleEveryOther :: [Integer] -> [Integer]
+--doubleEveryOther x = reverse $ doubleEverySecond $ reverse x
+--  where doubleEverySecond (x1 : x2 : xs) = [x1, x2 * 2] ++ (doubleEverySecond xs)
+--        doubleEverySecond (x : []) = [x]
+--        doubleEverySecond xs =  xs
+--        -- we replace the last 2 pattern matches with:
+--        -- doubleEverySecond xs = xs
+--        -- we should use : instead of ++ in the first pattern match:
+--        -- x1 : x2 * 2 : doubleEverySecond xsc
+--        -- we should name the function doubleEveryOther' instead of doubleEverySecond
+
+--doubleEveryOther :: [Integer] -> [Integer]
+--doubleEveryOther xs = reverse $ go $ reverse xs
+--    where
+--        go :: [Integer] -> [Integer]
+--        go []       = []
+--        go (x:[])   = [x]
+--        go (x:y:xs) = x:y*2:go xs
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther x = reverse $ doubleEverySecond $ reverse x
-  where doubleEverySecond (x1 : x2 : xs) = [x1, x2 * 2] ++ (doubleEverySecond xs)
-        doubleEverySecond (x : []) = [x]
-        doubleEverySecond xs =  xs
-        -- we replace the last 2 pattern matches with:
-        -- doubleEverySecond xs = xs
-        -- we should use : instead of ++ in the first pattern match:
-        -- x1 : x2 * 2 : doubleEverySecond xs
-        -- we should name the function doubleEveryOther' instead of doubleEverySecond
+doubleEveryOther = reverse . zipWith (*) (cycle [1, 2]) . reverse
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
@@ -32,7 +45,9 @@ sumDigits (x:xs)
   -- we can instead not use ++ operator by having this line as:
   -- | x >= 10 = sumDigits (toDigits x) + sumDigits xs
 
-validate :: Integer -> Bool
-validate x = (sumDigits $ doubleEveryOther $ toDigits x) `rem` 10 == 0
+--validate :: Integer -> Bool
+--validate x = (sumDigits $ doubleEveryOther $ toDigits x) `rem` 10 == 0
 -- we can use composing here and eta reduction here too!
--- validate = (==0).(`rem`10).sumDigits.doubleEveryOther.toDigits
+
+validate :: Integer -> Bool
+validate = (==0) . ((flip mod) 10) . sumDigits . doubleEveryOther . toDigits
